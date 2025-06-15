46e20a4ac10021a5e69fd42490dd5140172216bd
doshchuk-ms <Doshchuk-MS@rshbintech.ru>
2025-06-15 23:28:05 +0700

    Основные конструкции языка

--- homework/1.3/1.3.1/main.go
+++ homework/1.3/1.3.1/main.go
@@ -7,5 +7,5 @@ var age int = 30
 var name string = "Иван"
 var height float64 = 1.75
 var message bool= true
-fmt.Println(AppName, "\n", "Профиль пользователя:\n", "Имя:",name,"\n", "Возраст:",age,"лет", "\n", "Рост:",height,"м","\n", "Подписан на рассылку:",message )
+fmt.Println(AppName, "\n", "Профиль пользователя:\n", "Имя:\n",name, "Возраст:",age,"лет", "\n", "Рост:",height,"м","\n", "Подписан на рассылку:",message )
 }
